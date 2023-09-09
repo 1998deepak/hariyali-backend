c8e2b05b455140d2505e0c6a9616dfe0212e5af1
prashant <khacpr@mahindra.com>
2023-09-09 14:36:53 +0530

    Changes added for winter

--- src/main/java/com/hariyali/serviceimpl/PlantationServiceImpl.java
+++ src/main/java/com/hariyali/serviceimpl/PlantationServiceImpl.java
@@ -63,7 +63,6 @@ public class PlantationServiceImpl implements PlantationService {
 
 	@Autowired
 	private DonationRepository donationRepository;
-	
 
 	@Autowired
 	private UserPackageRepository userPackageRepository;
@@ -80,93 +79,89 @@ public class PlantationServiceImpl implements PlantationService {
 	@Override
 	public ByteArrayInputStream exportExcelUserPlant(String seasonType) {
 
-	    Workbook workbook = new SXSSFWorkbook();
+		Workbook workbook = new SXSSFWorkbook();
 
-	    try {
-	        Sheet sheet = workbook.createSheet("User Plant Report ");
+		try {
+			Sheet sheet = workbook.createSheet("User Plant Report ");
 
-	        Row row = sheet.createRow(0);
-	        CellStyle style = workbook.createCellStyle();
-	        XSSFFont font = (XSSFFont) workbook.createFont();
-	        font.setBold(true);
-	        font.setFontHeight(12);
-	        style.setFont(font);
-	        
-	        
-	        
-	        // Set the background color directly (YELLOW)
-	        style.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
-	        style.setFillPattern((short) FillPatternType.SOLID_FOREGROUND.ordinal());
-	        
-	       
+			Row row = sheet.createRow(0);
+			CellStyle style = workbook.createCellStyle();
+			XSSFFont font = (XSSFFont) workbook.createFont();
+			font.setBold(true);
+			font.setFontHeight(12);
+			style.setFont(font);
 
-	        Cell cell = row.createCell(0);
-	        cell.setCellValue("State");
-	        sheet.autoSizeColumn(0);
-	        cell.setCellStyle(style);
+			// Set the background color directly (YELLOW)
+			style.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+			style.setFillPattern((short) FillPatternType.SOLID_FOREGROUND.ordinal());
 
-	        cell = row.createCell(1);
-	        cell.setCellValue("District");
-	        sheet.autoSizeColumn(1);
-	        cell.setCellStyle(style);
+			Cell cell = row.createCell(0);
+			cell.setCellValue("State");
+			sheet.autoSizeColumn(0);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(2);
-	        cell.setCellValue("Village");
-	        sheet.autoSizeColumn(2);
-	        cell.setCellStyle(style);
+			cell = row.createCell(1);
+			cell.setCellValue("District");
+			sheet.autoSizeColumn(1);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(3);
-	        cell.setCellValue("Season");
-	        sheet.autoSizeColumn(3);
-	        cell.setCellStyle(style);
+			cell = row.createCell(2);
+			cell.setCellValue("Village");
+			sheet.autoSizeColumn(2);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(4);
-	        cell.setCellValue("Plot");
-	        sheet.autoSizeColumn(4);
-	        cell.setCellStyle(style);
+			cell = row.createCell(3);
+			cell.setCellValue("Season");
+			sheet.autoSizeColumn(3);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(5);
-	        cell.setCellValue("NoOfPlantsPlanted");
-	        sheet.autoSizeColumn(5);
-	        cell.setCellStyle(style);
+			cell = row.createCell(4);
+			cell.setCellValue("Plot");
+			sheet.autoSizeColumn(4);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(6);
-	        cell.setCellValue("Plantation Date");
-	        sheet.autoSizeColumn(6);
-	        cell.setCellStyle(style);
+			cell = row.createCell(5);
+			cell.setCellValue("NoOfPlantsPlanted");
+			sheet.autoSizeColumn(5);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(7);
-	        cell.setCellValue("Lattitude");
-	        sheet.autoSizeColumn(7);
-	        cell.setCellStyle(style);
+			cell = row.createCell(6);
+			cell.setCellValue("Plantation Date");
+			sheet.autoSizeColumn(6);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(8);
-	        cell.setCellValue("Longitude");
-	        sheet.autoSizeColumn(8);
-	        cell.setCellStyle(style);
+			cell = row.createCell(7);
+			cell.setCellValue("Lattitude");
+			sheet.autoSizeColumn(7);
+			cell.setCellStyle(style);
 
-	        cell = row.createCell(9);
-	        cell.setCellValue("Status");
-	        sheet.autoSizeColumn(9);
-	        cell.setCellStyle(style);
+			cell = row.createCell(8);
+			cell.setCellValue("Longitude");
+			sheet.autoSizeColumn(8);
+			cell.setCellStyle(style);
 
-	        row = sheet.createRow(1);
-	        cell = row.createCell(3);
-	        cell.setCellValue(seasonType);
-	        
-	        // Auto-size columns
-	        for (int i = 0; i <= 9; i++) {
-	            sheet.autoSizeColumn(i);
-	        }
+			cell = row.createCell(9);
+			cell.setCellValue("Status");
+			sheet.autoSizeColumn(9);
+			cell.setCellStyle(style);
 
-	        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	        workbook.write(outputStream);
-	        return new ByteArrayInputStream(outputStream.toByteArray());
+			row = sheet.createRow(1);
+			cell = row.createCell(3);
+			cell.setCellValue(seasonType);
 
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return null;
+			// Auto-size columns
+			for (int i = 0; i <= 9; i++) {
+				sheet.autoSizeColumn(i);
+			}
+
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			workbook.write(outputStream);
+			return new ByteArrayInputStream(outputStream.toByteArray());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 //	@Override
@@ -331,8 +326,7 @@ public class PlantationServiceImpl implements PlantationService {
 //		}
 //		return "Success";
 //	}
-	
-	
+
 	@Override
 	public String uploadPlantationExcel(XSSFWorkbook workbook) {
 		int countRow = 0;
@@ -364,73 +358,100 @@ public class PlantationServiceImpl implements PlantationService {
 			}
 			System.out.println(excelUserPlantationDTOs.get(0).getSeason());
 			if (excelUserPlantationDTOs.get(0).getSeason().equalsIgnoreCase("MONSOON")) {
-				Long noOfPlantsPlanted = excelUserPlantationDTOs.stream()
-						.collect(Collectors.summingLong(ExcelUserPlantationDTO::getNoOfPlantsPlanted));
+				// if season type is mansoon
+				// LocalDate.now().getYear() '2021-04-01' AND '2022-03-31'
 
-				List<UserPlantationAndDonationDTO> userPlantationAndDonationDTO = mapper.convertValue(
-						plantationRepository.getUserDonationAndPlantationData(),
-						new TypeReference<List<UserPlantationAndDonationDTO>>() {
-						});
-
-				Long sum = userPlantationAndDonationDTO.stream()
-						.collect(Collectors.summingLong(UserPlantationAndDonationDTO::getNoOfBuckets));
-				if (noOfPlantsPlanted.equals(sum)) {
-					long noPlantsPlantedTemp = 0;
-					for (ExcelUserPlantationDTO dto : excelUserPlantationDTOs) {
-						noPlantsPlantedTemp = dto.getNoOfPlantsPlanted();
-						List<UserPlantationAndDonationDTO> userPlantationAndDonationDTOs = mapper.convertValue(
-								plantationRepository.getUserDonationAndPlantationData(),
-								new TypeReference<List<UserPlantationAndDonationDTO>>() {
-								});
-						for (UserPlantationAndDonationDTO plantationAndDonationDTO : userPlantationAndDonationDTOs) {
-							Plantation plantation = new Plantation();
-							plantation.setNoOfplantsPlanted(plantationAndDonationDTO.getNoOfBuckets());
-							noPlantsPlantedTemp -= plantationAndDonationDTO.getNoOfBuckets();
-							plantation.setDistrict(dto.getDistrict());
-							plantation.setState(dto.getState());
-							plantation.setSeason(dto.getSeason());
-							plantation.setFinacialYear(LocalDate.now().getYear());
-							plantation.setPlot(dto.getPlot());
-							plantation.setLongitude(dto.getLongitude());
-							plantation.setLattitude(dto.getLattitude());
-							System.out.println("Plantation date=" + dto.getPlantationDate());
-							plantation.setPlantationDate(LocalDate.now());
-							plantation.setStatus(dto.getStatus());
-							plantation.setVillage(dto.getVillage());
-							plantation.setUserPackages(
-									userPackageRepository.findById(plantationAndDonationDTO.getPackages()).get());
-							plantationRepository.save(plantation);
-							userPackageRepository.update(plantationAndDonationDTO.getPackages());
-							if (noPlantsPlantedTemp == 0) {
-								break;
-							}
-						}
-					}
-
-					for (ExcelUserPlantationDTO dto : excelUserPlantationDTOs) {
-						PlantationMaster plantationMaster = new PlantationMaster();
-						plantationMaster.setDistrict(dto.getDistrict());
-						plantationMaster.setState(dto.getState());
-						plantationMaster.setLattitude(dto.getLattitude());
-						plantationMaster.setLongitude(dto.getLongitude());
-						plantationMaster.setVillage(dto.getVillage());
-						plantationMaster.setPlot(dto.getPlot());
-						plantationMaster.setSeason(dto.getSeason());
-						plantationMaster.setNoOfPlantsPlanted(dto.getNoOfPlantsPlanted());
-						plantationMasterRepository.save(plantationMaster);
-
-					}
-
-				} else {
-					return "Number of plants Planted is not equal to number of plants recived in current plantation year";
-				}
-
+				int currentYear = LocalDate.now().getYear();
+				@SuppressWarnings("removal")
+				String  seasonStartDate = new Integer(currentYear).toString() + "-04-01";
+				@SuppressWarnings("removal")
+				String  seasonEndDate = new Integer((currentYear + 1)).toString() + "-03-31";
+				System.out.println( seasonStartDate + ":" +  seasonEndDate);
+				String seasonType = "MONSOON";
+				return uploadExcel(excelUserPlantationDTOs, seasonType,  seasonStartDate,  seasonEndDate);
+				
+			} else if (excelUserPlantationDTOs.get(0).getSeason().equalsIgnoreCase("WINTER")) {
+				// if season type is (Winter) LocalDate.now().getYear() 1st sept 2021 to 31st
+				// August 2022
+				int currentYear = LocalDate.now().getYear();
+				String  seasonStartDate = new Integer(currentYear).toString() + "-09-01";
+				String  seasonEndDate = new Integer((currentYear + 1)).toString() + "-08-31";
+				System.out.println( seasonStartDate + ":" +  seasonEndDate);
+				String seasonType = "WINTER";
+				return uploadExcel(excelUserPlantationDTOs, seasonType,  seasonStartDate,  seasonEndDate);
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "Success";
+	}
+
+	private String uploadExcel(List<ExcelUserPlantationDTO> excelUserPlantationDTOs, String seasonType,
+			String  seasonStartDate, String  seasonEndDate) {
+
+		Long noOfPlantsPlanted = excelUserPlantationDTOs.stream()
+				.collect(Collectors.summingLong(ExcelUserPlantationDTO::getNoOfPlantsPlanted));
+
+		List<UserPlantationAndDonationDTO> userPlantationAndDonationDTO = mapper.convertValue(
+				plantationRepository.getUserDonationAndPlantationData(seasonType,seasonStartDate,seasonEndDate),
+				new TypeReference<List<UserPlantationAndDonationDTO>>() {
+				});
+
+		Long sum = userPlantationAndDonationDTO.stream()
+				.collect(Collectors.summingLong(UserPlantationAndDonationDTO::getNoOfBuckets));
+		if (noOfPlantsPlanted.equals(sum)) {
+			long noPlantsPlantedTemp = 0;
+			for (ExcelUserPlantationDTO dto : excelUserPlantationDTOs) {
+				noPlantsPlantedTemp = dto.getNoOfPlantsPlanted();
+				List<UserPlantationAndDonationDTO> userPlantationAndDonationDTOs = mapper.convertValue(
+						plantationRepository.getUserDonationAndPlantationData(seasonType,seasonStartDate,seasonEndDate),
+						new TypeReference<List<UserPlantationAndDonationDTO>>() {
+						});
+				for (UserPlantationAndDonationDTO plantationAndDonationDTO : userPlantationAndDonationDTOs) {
+					Plantation plantation = new Plantation();
+					plantation.setNoOfplantsPlanted(plantationAndDonationDTO.getNoOfBuckets());
+					noPlantsPlantedTemp -= plantationAndDonationDTO.getNoOfBuckets();
+					plantation.setDistrict(dto.getDistrict());
+					plantation.setState(dto.getState());
+					plantation.setSeason(dto.getSeason());
+					plantation.setFinacialYear(LocalDate.now().getYear());
+					plantation.setPlot(dto.getPlot());
+					plantation.setLongitude(dto.getLongitude());
+					plantation.setLattitude(dto.getLattitude());
+					System.out.println("Plantation date=" + dto.getPlantationDate());
+					plantation.setPlantationDate(LocalDate.now());
+					plantation.setStatus(dto.getStatus());
+					plantation.setVillage(dto.getVillage());
+					plantation.setUserPackages(
+							userPackageRepository.findById(plantationAndDonationDTO.getPackages()).get());
+					plantationRepository.save(plantation);
+					userPackageRepository.update(plantationAndDonationDTO.getPackages());
+					if (noPlantsPlantedTemp == 0) {
+						break;
+					}
+				}
+			}
+
+			for (ExcelUserPlantationDTO dto : excelUserPlantationDTOs) {
+				PlantationMaster plantationMaster = new PlantationMaster();
+				plantationMaster.setDistrict(dto.getDistrict());
+				plantationMaster.setState(dto.getState());
+				plantationMaster.setLattitude(dto.getLattitude());
+				plantationMaster.setLongitude(dto.getLongitude());
+				plantationMaster.setVillage(dto.getVillage());
+				plantationMaster.setPlot(dto.getPlot());
+				plantationMaster.setSeason(dto.getSeason());
+				plantationMaster.setNoOfPlantsPlanted(dto.getNoOfPlantsPlanted());
+				plantationMasterRepository.save(plantationMaster);
+
+			}
+
+		} else {
+			return "Number of plants Planted is not equal to number of plants recived in current plantation year";
+		}
+		return "Success";
+
 	}
 
 	private void sendMail(String userName) {
@@ -452,13 +473,14 @@ public class PlantationServiceImpl implements PlantationService {
 
 	@Override
 	public ApiResponse<Object> getAllPlantationMaster() {
-		
+
 		ApiResponse<Object> response = new ApiResponse<>();
 
-		Optional<List<PlantationMaster>> plantationMasterOptional = ofNullable(this.plantationMasterRepository.findAll());
+		Optional<List<PlantationMaster>> plantationMasterOptional = ofNullable(
+				this.plantationMasterRepository.findAll());
 
 		if (plantationMasterOptional.isPresent()) {
-			
+
 			response.setData(plantationMasterOptional);
 			response.setStatus(EnumConstants.SUCCESS);
 			response.setStatusCode(HttpStatus.OK.value());
@@ -468,7 +490,7 @@ public class PlantationServiceImpl implements PlantationService {
 		} else {
 			throw new CustomException("plantation data Doesn't Exist.");
 		}
-		
+
 	}
 
 }
