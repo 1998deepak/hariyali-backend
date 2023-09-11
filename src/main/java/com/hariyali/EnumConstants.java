32416ae55a39da3eaf207f0d964d148b406aa7c3
deepakrios <deepak.tiwade@kriosispl.in>
2023-09-11 19:57:51 +0530

    new code merge changes for VAPT

--- src/main/java/com/hariyali/EnumConstants.java
+++ src/main/java/com/hariyali/EnumConstants.java
@@ -31,43 +31,26 @@ public interface EnumConstants {
 
 	public static final String receiptNumber = "txn_23" + donor;
 
-	public static final String GIFTING_MSG_BODY = "Dear Sponsor,\n" +
-            "\t\t Welcome to Project “Hariyali”.\n" +
-            "On the occasion of your %s, you have been gifted with Hariyali sponsorship to make your day special.\n" +
-            "The gift certificate is attached herewith.\n" +
-            " \tYou can login to your account on our website www.hariyali.org.in with your login details.\n" +
-            "Email ID : %s\n" +
-            "Password : %s\n" +
-            "\n" +
-            "Mahindra Foundation\n" +
-            "Sheetal Mehta\n" +
-            "Trustee & Executive Director\n" +
-            "K.C. Mahindra Education Trust,\n" +
-            "3rd Floor, Cecil Court,\n" +
-            "Near Regal Cinema,\n" +
-            "Mahakavi Bushan Marg,\n" +
-            "Mumbai 400001\n" +
-            "PS : Contact support@hariyali.org.in in case of any query.";
+	public static final String GIFTING_MSG_BODY = "Dear Sponsor,<br><p>&nbsp;Welcome to Project &ldquo;Hariyali&rdquo;.<br>"
+			+ "&nbsp;On the occasion of your %s, you have been gifted with Hariyali sponsorship to make your day special.<br>"
+			+ "The gift certificate is attached herewith.<br></p>"
+			+ "&nbsp;You can login to your account on our website www.hariyali.org.in with your login details.<br>"
+			+ "Email ID : %s<br>" + "Password : %s<br>" + "<br>" + "Mahindra Foundation<br>" + "Sheetal Mehta<br>"
+			+ "Trustee & Executive Director<br>" + "K.C. Mahindra Education Trust,<br>" + "3rd Floor, Cecil Court,<br>"
+			+ "Near Regal Cinema,<br>" + "Mahakavi Bushan Marg,<br>" + "Mumbai 400001<br>"
+			+ "PS : Contact support@hariyali.org.in in case of any query.";
 
 	public static final String GIFTING_MSG_SUBJECT = "Someone wanted you to have this special gift";
 	
 	
 	String subject = "Welcome To Hariyali";
-	String content = "Dear Sponsor,\n\tWelcome to Project “Hariyali”.\n"
-            + "The Mahindra Foundation and Naandi Foundation would like to thank you for your donation.\n"
-            + "You can login to your account on our website www.hariyali.org.in with your login details.\n"
-            + "Email ID : %s\n"
-            + "Password : %s\n"
-            + "\tThanking you for your support to Project Hariyali.\n"
-            + "Mahindra Foundation\n"
-            + "Sheetal Mehta\n"
-            + "Trustee & Executive Director\n"
-            + "K.C. Mahindra Education Trust,\n"
-            + "3rd Floor, Cecil Court,\n"
-            + "Near Regal Cinema,\n"
-            + "Mahakavi Bushan Marg,\n"
-            + "Mumbai 400001\n"
-            + "PS : Contact support@hariyali.org.in in case of any query.";
-	
+	String content="<p>Dear Sponsor,</p>" +
+			"<p>&nbsp;&nbsp;Welcome to Project \"Hariyali\".</p>" +
+			"<p>&nbsp;The Mahindra Foundation and Naandi Foundation would like to thank you for your donation.You can login to your account on our website "
+			+"<a href='http://www.hariyali.org.in'>www.hariyali.org.in</a> with your login details.</p>"
+			+"<p>Email ID :%s<br>"
+			+"Password :%s</p>"
+			+"<p>&nbsp;Thanking you for your support to Project Hariyali.</p>"
+			+"<p>PS : Contact <a href='mailto:support@hariyali.org.in'>support@hariyali.org.in</a> in case of any query.</p>";
 
 }
--- src/main/java/com/hariyali/utils/EmailService.java
+++ src/main/java/com/hariyali/utils/EmailService.java
@@ -1,5 +1,6 @@
 package com.hariyali.utils;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -17,134 +18,81 @@ import org.springframework.stereotype.Service;
 import com.hariyali.EnumConstants;
 import com.hariyali.entity.Receipt;
 import com.hariyali.entity.Users;
-import com.hariyali.repository.ReceiptRepository;
+import com.hariyali.exceptions.EmailNotConfiguredException;
+import com.hariyali.serviceimpl.CCServiceEmailAPI;
 
 @Service
 public class EmailService {
 
 	@Autowired
-	private JavaMailSender mailSender;
-
-	@Value("${spring.mail.username}")
-	private String fromEmail;
-
-	@Autowired
-	ReceiptRepository receiptRepository;
+	CCServiceEmailAPI ccServiceEmailAPI;
 
 	public void sendSimpleEmail(String toEmail, String subject, String body) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom(fromEmail);
-		message.setTo(toEmail);
-		message.setText(body);
-		message.setSubject(subject);
-		mailSender.send(message);
+		ccServiceEmailAPI.sendCorrespondenceMail(toEmail, subject, body);
 		System.out.println("Mail Sent...");
 
 	}
 
 	public void sendSimpleEmailToHariyaliTeam(String toEmail, String subject, String body) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom(toEmail);
-		message.setTo(fromEmail);
-		message.setText(body);
-		message.setSubject(subject);
-		mailSender.send(message);
+		ccServiceEmailAPI.sendSupportMail(toEmail, subject, body);
 		System.out.println("Mail Sent To Hariyali Team ...");
 
 	}
 
-	public void sendEmailWithAttachment(String to, String subject, String text, String attachmentPath, Users user)
-			throws MessagingException {
-		MimeMessage mimeMessage = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-		helper.setTo(to);
-		helper.setSubject(subject);
-		helper.setText(String.format(text, user.getEmailId(),user.getPassword()));
-		FileSystemResource file = new FileSystemResource(attachmentPath);
-		helper.addAttachment(file.getFilename(), file, "application/pdf");
-		mailSender.send(mimeMessage);
-		System.out.println("Mail send");
-	}
-	
-	public void sendWelcomeLetterMail(String to, String subject, String text,  Users user)
-			throws MessagingException {
-		MimeMessage mimeMessage = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-		helper.setTo(to);
-		helper.setSubject(subject);
-		helper.setText(String.format(text, user.getEmailId(),user.getPassword()));
+//	public void sendEmailWithAttachment(String to, String subject, String text, String attachmentPath, Users user)
+//			throws MessagingException {
+//		MimeMessage mimeMessage = mailSender.createMimeMessage();
+//		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+//		helper.setTo(to);
+//		helper.setSubject(subject);
+//		helper.setText(String.format(text, user.getEmailId(),user.getPassword()));
 //		FileSystemResource file = new FileSystemResource(attachmentPath);
 //		helper.addAttachment(file.getFilename(), file, "application/pdf");
-		mailSender.send(mimeMessage);
+//		mailSender.send(mimeMessage);
+//		System.out.println("Mail send");
+//	}
+
+	public void sendWelcomeLetterMail(String to, String subject, String text, Users user) {
+		String body = String.format(text, user.getEmailId(), user.getPassword());
+		ccServiceEmailAPI.sendCorrespondenceMail(to, subject, body);
 		System.out.println("Mail send");
 	}
 
 	public void sendGiftingLetterEmail(Users recipientData,String donationEvent) {
 		String subject = EnumConstants.GIFTING_MSG_SUBJECT;
 		String body = EnumConstants.GIFTING_MSG_BODY;
-		String mailBody = String.format(body,donationEvent, recipientData.getEmailId(),recipientData.getPassword());
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom(fromEmail);
-		message.setTo(recipientData.getEmailId());
-		message.setText(mailBody);
-		message.setSubject(subject);
-		mailSender.send(message);
+		String mailBody = String.format(body, donationEvent, recipientData.getEmailId(), recipientData.getPassword());
+		ccServiceEmailAPI.sendCorrespondenceMail(recipientData.getEmailId(), subject, mailBody);
 		System.out.println("Mail Sent...");
 	}
 
 	public void sendWebIdEmail(String toEmail, Users user) {
-		String body = "Dear Sir/Madam,\n \tWelcome to Project Hariyali."
-				+ "The Mahindra Foundation,would like to thank you for your donation to Project Hariyali. The main objective of the project is to do 5 Billion Tree Plantation from 2026 in several parts of the Nation. "
-				+ "The Tree Plantation is the main Agenda of the Project. "
-				+ "The HARIYALI is a Partnership between Mahindra and Mahindra and the Nandi Foundation. The Project will be jointly managed by M&M and Nandi Foundation. \r\n"
-				+ "Below is your Web Id :" + user.getWebId() + "\rWait For Admin Approval.\\r\\n"
-				+ "\nBest wishes,\nTeam Hariyali\r\n" + "\r\n";
-		SimpleMailMessage message = new SimpleMailMessage();
-
-		message.setFrom(fromEmail);
-		message.setTo(toEmail);
-		message.setSubject("Web Id for Plant Donation");
-		message.setText(body);
-		mailSender.send(message);
-
+		String body = "Dear Sponsor,<br> <p>Welcome to Project Hariyali.</p>"
+				+ "The Mahindra Foundation and Naandi Foundation would like to thank you for your donation."
+				+ "Below is your Web Id :<b>" + user.getWebId() + "</b><br>Wait For Admin Approval.<br>"
+				+ "<br>Best wishes,<br>Team Hariyali<br>" + "<br>";
+		try {
+			ccServiceEmailAPI.sendCorrespondenceMail(toEmail, "Web Id for Plant Donation", body);
+		} catch (EmailNotConfiguredException e) {
+			throw new EmailNotConfiguredException(e.getMessage());
+		}
 		System.err.println("Mail send successfully");
 
 	}
 
 	public void sendPlantationEmail(String toEmail, String subject, String bodyMessage) {
-		MimeMessage mimeMessage = mailSender.createMimeMessage();
-		try {
-			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-			helper.setTo(toEmail);
-			helper.setSubject(subject);
-			helper.setText(bodyMessage, true); // Set to true for HTML content if needed
-
-			mailSender.send(mimeMessage);
-		} catch (Exception e) {
-			// Handle any exceptions here
-			e.printStackTrace();
-		}
+		ccServiceEmailAPI.sendCorrespondenceMail(toEmail, subject, bodyMessage);
 	}
 
-	public void sendReceiptWithAttachment(String to, Receipt receipt) throws MessagingException {
+	public void sendReceiptWithAttachment(String to, Receipt receipt) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        String formattedDate = sdf.format(receipt.getRecieptDate());
-		String text = "Dear Sponsor,\n" +
-	            "\t\tWe thank you for your sponsorship.\n" +
-	            "Rec.No:"+ receipt.getRecieptNumber() +" Date:"+ formattedDate+"\n"+
-	            "Please find a PDF version of the receipt attached herewith.\n" +
-	            "Thanking you for your support to project Hariyali.\n" +
-	            "\n" +
-	            "Naandi Foundation\n" +
-	            "\tAddress : \n" +
-	            "PS : Contact 'support@hariyali.org.in' in case of any query.";
-		MimeMessage mimeMessage = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-		helper.setTo(to);
-		helper.setSubject("Receipt For Your Donation");
-		helper.setText(text);
-		FileSystemResource file = new FileSystemResource(receipt.getReciept_Path());
-		helper.addAttachment(file.getFilename(), file, "application/pdf");
-		mailSender.send(mimeMessage);
+		String formattedDate = sdf.format(receipt.getRecieptDate());
+		FileSystemResource resource = new FileSystemResource(receipt.getReciept_Path());
+		File[] files = {resource.getFile()};
+		String text = "Dear Sponsor,<br>" + "<p>We thank you for your sponsorship.<br>" + "Rec.No:"
+				+ receipt.getRecieptNumber() + " Date:" + formattedDate + "<br></p>"
+				+ "Please find a PDF version of the receipt attached herewith.<br>"
+				+ "<p>Thanking you for your support to project Hariyali.</p><br>Naandi Foundation<br>Address : <br>PS : Contact 'support@hariyali.org.in' in case of any query.";
+		ccServiceEmailAPI.sendPaymentsMail(to, "Receipt For Your Donation", text,files);
 	}
 }
