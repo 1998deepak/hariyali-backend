25a9807483351e5ee28293189c7c3a290949c0db
VinodSatavase <vinodsatavase@gmail.com>
2023-09-17 07:07:23 +0530

    Commit with planation process and planatation page

--- src/main/java/com/hariyali/dto/PlantationMasterDTO.java
+++ src/main/java/com/hariyali/dto/PlantationMasterDTO.java
@@ -5,23 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlantationMasterDTO {
-	
 
-	private long id;
-	private String state;
-	private String district;
-	private String village;
-	private String season;
-	private String plot;
-	private Long noOfPlantsPlanted;
-	private String plantationDate;
-	private Float lattitude;
-	private Float longitude;
-	private String status;
+    private long id;
+    private String state;
+    private String district;
+    private String village;
+    private String season;
+    private String plot;
+    private Long noOfPlantsPlanted;
+    private Date plantationDate;
+    private String plantationDateString;
+    private Double latitude;
+    private Double longitude;
+    private String status;
+    private Integer plantationYear;
 
-}
+}//class
