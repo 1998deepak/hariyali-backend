90e0426f1ccf14bde3648da053489a4462b55b8b
VinodSatavase <vinodsatavase@gmail.com>
2023-09-17 21:23:24 +0530

    Commit with commitment page

--- src/main/java/com/hariyali/service/PlantationService.java
+++ src/main/java/com/hariyali/service/PlantationService.java
@@ -1,21 +1,21 @@
-package com.hariyali.service;
-
-import java.io.ByteArrayInputStream;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import com.hariyali.dto.ApiResponse;
-import com.hariyali.entity.Plantation;
-
-public interface PlantationService {
-
-	public ByteArrayInputStream exportExcelUserPlant(String seasonType);
-
-	public String uploadPlantationExcel(XSSFWorkbook workbook);
-
-	public List<Plantation> getPlantationsByDonationId(Long donationId);
-	
-	public Map<String, Object> getAllPlantationMaster(Integer page, Integer size);
-}
+//package com.hariyali.service;
+//
+//import java.io.ByteArrayInputStream;
+//import java.util.List;
+//import java.util.Map;
+//
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//
+//import com.hariyali.dto.ApiResponse;
+//import com.hariyali.entity.Plantation;
+//
+//public interface PlantationService {
+//
+//	public ByteArrayInputStream exportExcelUserPlant(String seasonType);
+//
+//	public String uploadPlantationExcel(XSSFWorkbook workbook);
+//
+//	public List<Plantation> getPlantationsByDonationId(Long donationId);
+//
+//	public Map<String, Object> getAllPlantationMaster(Integer page, Integer size);
+//}
