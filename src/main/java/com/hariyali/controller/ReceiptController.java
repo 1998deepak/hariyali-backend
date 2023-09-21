b4d05f19fc5fb16925e43e01aef081e8c421488f
SuvarnaKhatale <98016936+SuvarnaKhatale@users.noreply.github.com>
2023-09-21 13:07:11 +0530

    Update ReceiptController.java

--- src/main/java/com/hariyali/controller/ReceiptController.java
+++ src/main/java/com/hariyali/controller/ReceiptController.java
@@ -33,10 +33,6 @@ public class ReceiptController {
 
 	@Autowired
 	ReceiptService receiptService;
-	@Autowired
-	private JavaMailSender javaMailSender;
-	@Value("${spring.mail.username}")
-	private String fromEmail;
 
 	@Autowired
 	ReceiptRepository receiptRepository;
@@ -45,41 +41,6 @@ public class ReceiptController {
 	@GetMapping("/getAllReceipt")
 	public ResponseEntity<ApiResponse<Object>> getAllReceipt() {
 		return new ResponseEntity<>(receiptService.getAllReceipt(), HttpStatus.OK);
-	}
-
-	@PostMapping("/send")
-	public ResponseEntity<String> sendEmailWithAttachment(@RequestParam("to") String to,
-			@RequestParam("subject") String subject, @RequestParam("text") String text,
-			@RequestParam("receiptId") int donationId) {
-
-		try {
-			// Fetch the receipt from the database
-			Receipt receipt = receiptRepository.getReceiptByDonation(donationId);
-//			if (!optionalReceipt.isPresent()) {
-//				return ResponseEntity.badRequest().body("Receipt not found.");
-//			}
-//			Receipt receipt = optionalReceipt.get();
-
-			// Create and send the email with attachment
-			MimeMessage message = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
-
-			helper.setTo(to);
-			helper.setSubject(subject);
-			helper.setText(text);
-
-			// Attach the file to the email
-			String fileLocation = receipt.getReciept_Path();
-			FileSystemResource fileResource = new FileSystemResource(fileLocation);
-			helper.addAttachment(fileResource.getFilename(), fileResource, "application/pdf");
-
-			javaMailSender.send(message);
-
-			return ResponseEntity.ok("Email sent successfully with attachment.");
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body("Error sending email: " + e.getMessage());
-		}
 	}
 
 	@GetMapping("/receipt/download/{recieptNumber}")
