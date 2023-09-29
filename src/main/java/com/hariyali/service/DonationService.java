4a9c7795df6fd0442485049caefd07e40fb8725c
Shivanand Ghule <ghule.shivanand2@mahindra.com>
2023-09-29 20:40:50 +0530

    generate certificate thank you added

--- src/main/java/com/hariyali/service/DonationService.java
+++ src/main/java/com/hariyali/service/DonationService.java
@@ -41,4 +41,6 @@ public interface DonationService {
 	
 	public Map<String,String> generateCertificate(String recipientName ,String messageContent,String donationEvent,String donarName,String emailID);
 
+	Map<String, String> generateCertificateForThankYou(String donarName, String emailID);
+
 }
