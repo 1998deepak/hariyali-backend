8a79da69d2be14c5ee74e62e0717cc64a9153440
prashant <khacpr@mahindra.com>
2023-09-30 00:09:39 +0530

    All the text in upper format

--- src/main/java/com/hariyali/serviceimpl/MasterServiceImpl.java
+++ src/main/java/com/hariyali/serviceimpl/MasterServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hariyali.serviceimpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,18 +29,29 @@ public class MasterServiceImpl implements MasterService {
 	@Override
 	public List<Country> getAllCountry() {
 
-		return countryRepository.findByIsActive(true);
+		return countryRepository.findByIsActive(true).stream()
+				.map(c -> new Country(c.getId(), c.getCreatedBy(), c.getCreatedDate(), c.getIsActive(),
+						c.getUpdatedBy(), c.getUpdatedDate(), c.getCountryCode(), c.getCountryName().toUpperCase()))
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public List<State> getAllStateByCountryId(String countryCode) {
 
-		return stateRepository.findByCountryCode(countryCode);
+		return stateRepository.findByCountryCode(countryCode).stream()
+				.map(s -> new State(s.getId(), s.getCreatedBy(), s.getCreatedDate(), s.getIsActive(), s.getUpdatedBy(),
+						s.getUpdatedDate(), s.getCountryCode(), s.getCountryName(), s.getCountryId(), s.getStateCode(),
+						s.getStateName()))
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public List<Citzenship> getAllCitizensip() {
-		return citizenshipRepository.findAll();
+
+		return citizenshipRepository.findByIsActive(true).stream().map(c -> new Citzenship(c.getId(),
+				c.getCitizenshipName().toUpperCase(), null, c.getIsActive(), null, null, null))
+				.collect(Collectors.toList());
+
 	}
 
 }
